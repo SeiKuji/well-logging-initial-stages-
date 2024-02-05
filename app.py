5f8312339a526b4351cac92ed36cee2ccabcaf48
SeiKuji <127077559+SeiKuji@users.noreply.github.com>
2024-02-05 11:38:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,57 +20,16 @@ from striplog import Legend, Lexicon, Interval, Component, Decor
 
 
 #menu = option_menu()
-def streamlit_menu(example=1):
-    if example == 1:
-        # 1. as sidebar menu
-        with st.sidebar:
-            selected = option_menu(
-                menu_title="Main Menu",  # required
-                options=["Overview", "Creator", "Contact", "Plotting"],  # required
-                icons=["house", "book", "envelope", "bar-chart-line-fill"],  # optional
-                menu_icon="cast",  # optional
-                default_index=0,  # optional
-            )
-        return selected
 
-    if example == 2:
-        # 2. horizontal menu w/o custom style
-        selected = option_menu(
-            menu_title=None,  # required
-            options=["Overview", "Creator", "Contact", "Plotting"],  # required
-            icons=["house", "book", "envelope", "bar-chart-line-fill"],  # optional
-            menu_icon="cast",  # optional
-            default_index=0,  # optional
-            orientation="horizontal",
-        )
-        return selected
-
-    if example == 3:
-        # 2. horizontal menu with custom style
-        selected = option_menu(
-            menu_title=None,  # required
-            options=["Overview", "Creator", "Contact", "Plotting"],  # required
-            icons=["house", "book", "envelope", "bar-chart-line-fill"],  # optional
-            menu_icon="cast",  # optional
-            default_index=0,  # optional
-            orientation="horizontal",
-            styles={
-                "container": {"padding": "0!important", "background-color": "#fafafa"},
-                "icon": {"color": "orange", "font-size": "25px"},
-                "nav-link": {
-                    "font-size": "25px",
-                    "text-align": "left",
-                    "margin": "0px",
-                    "--hover-color": "#eee",
-                },
-                "nav-link-selected": {"background-color": "green"},
-            },
-        )
-        return selected
-st.title('This is first trial')
-EXAMPLE_NO = st.number_input('Choose from 1 to 3', max_value=3, value=1, step=1)
-selected = streamlit_menu(example=EXAMPLE_NO)
-
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Main Menu",  # required
+        options=["Overview", "Creator", "Contact", "Plotting"],  # required
+        icons=["house", "book", "envelope", "bar-chart-line-fill"],  # optional
+        menu_icon="cast",  # optional
+        default_index=0,  # optional
+    )
+    
 if selected == "Overview":
     st.title(selected)
     st.markdown("<h2 style='text-align: center; color: whitegrey; font-size: 30px'> Interpretation of Neutron-Density and Photoelectric Factor Well Log using Python Based Program", unsafe_allow_html=True)
